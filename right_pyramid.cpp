7e145595af317a66dfe7d0e44ed1b03e9bc79df3
Darshan Pakhale <darshanvpakhale26@gmail.com>
2024-03-24 23:46:54 +0530

    Initial Commit

---
+++ right_pyramid.cpp
@@ -0,0 +1,23 @@
+// Program to print right half pyramid
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows;
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= rows; j++) {
+            if (j <= rows - i) {
+                cout << "  ";
+            } else {
+                cout << "* ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
